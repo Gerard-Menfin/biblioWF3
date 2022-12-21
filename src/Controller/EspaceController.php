6ebf5ce00ec5d0734ce0f2276c3a30b5c7bb2201
Gerard-Menfin <gerard.mentor314@gmail.com>
2022-12-21 16:48:28 +0100

    sauvegarde du dernier jour de cours exemple de sauvegarde git

---
+++ src/Controller/EspaceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[IsGranted("ROLE_LECTEUR")]
+#[Route("/espace-lecteur")]
+class EspaceController extends AbstractController
+{
+    #[Route('/', name: 'app_espace')]
+    public function index(): Response
+    {
+        /* Si on veut récupérer les informations de l'utilisateur connecté dans un contrôleur, on utilise
+            $this->getUser()
+         Si aucun utilisateur n'est connecté, getUser() renvoie null */
+
+        $user = $this->getUser();
+        return $this->render('espace/index.html.twig');
+    }
+}
